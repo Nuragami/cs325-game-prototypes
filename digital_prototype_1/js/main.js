5dab99adb6ca3f621cf474a33f5565f3b55a8e14
Nuragami <michaelzhang.a@gmail.com>
2017-02-08 16:59:46 -0500

    r3

--- digital_prototype_1/js/main.js
+++ digital_prototype_1/js/main.js
@@ -2,7 +2,7 @@ window.onload = function ()
 {
     "use strict";
 
-    var game = new Phaser.Game(720, 1080, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+    var game = new Phaser.Game(400, 800, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
     function preload()
     {
@@ -11,34 +11,19 @@ window.onload = function ()
 
     }
 
-
     function create()
-    {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo(0.5, 0.5);
-
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable(bouncy, Phaser.Physics.ARCADE);
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text(game.world.centerX, 15, "Build something amazing.", style);
-        text.anchor.setTo(0.5, 0.0);
+    {       
+        players = game.add.group();
+        createPlayer();
     }
 
     function update()
     {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer(bouncy, this.game.input.activePointer, 500, 500, 500);
+      
+    }
+
+    function createPlayer()
+    {
+        var player = players.create(0,0, playersprite);
     }
 };
