37e7b4f7cb230fe38c5b7c56f502c38e0bcb7ad4
Nuragami <michaelzhang.a@gmail.com>
2017-02-08 18:20:05 -0500

    rr1

--- digital_prototype_1/js/main.js
+++ digital_prototype_1/js/main.js
@@ -1,51 +1,32 @@
-window.onload = function () {
+window.onload = function ()
+{
     "use strict";
 
     var game = new Phaser.Game(400, 800, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
-    var inputs;
+    
     var background;
-    var players;
+ 
 
-    function preload() {
+    function preload()
+    {
         // Load player sprite
         game.load.image('playersprite', "assets/playersprite.png");
         game.load.image('background', "assets/background.png");
 
     }
 
-    function create() {
+    function create()
+    {
         background = game.add.tileSprite(0, 0, 400, 800, 'background');
-
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-
-        players = game.add.group();
-        createPlayer(200, 200);
-        inputs = game.input.keyboard.createCursorKeys();
-
-        game.add.sprite(0, 0, 'playersprite')
+       
 
     }
 
-    function update() {
+    function update()
+    {
         background.tilePosition.y += 2;
-        playerUpdate();
+      
     }
 
-    function createPlayer(x, y) {
-        var player = players.create(x, y, 'playersprite');
-        player.body.colliderWorldBounds = true;
-
-    }
-
-    function playerUpdate() {
-        players.forEach(function (p) {
-            p.body.velocity.x = 0;
-            if (inputs.left.isDown) {
-                p.body.velocity.x = -150;
-            }
-            else if (inputs.right.isDown) {
-                p.body.velocity.x = 150;
-            }
-        });
-    }
+   
 };
