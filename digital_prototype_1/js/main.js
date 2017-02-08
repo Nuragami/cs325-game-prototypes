e3f3b588051d2281cadec6b5a441b4867400df87
Nuragami <michaelzhang.a@gmail.com>
2017-02-08 17:24:28 -0500

    r6

--- digital_prototype_1/js/main.js
+++ digital_prototype_1/js/main.js
@@ -1,29 +1,41 @@
-window.onload = function ()
-{
+window.onload = function () {
     "use strict";
 
     var game = new Phaser.Game(400, 800, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+    var inputs;
 
-    function preload()
-    {
+    function preload() {
         // Load player sprite
         game.load.image('playersprite', 'assets/playersprite.png');
 
     }
 
-    function create()
-    {       
+    function create() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         players = game.add.group();
-        createPlayer(200, 700);
+        createPlayer(200, 200);
+        inputs = game.input.keyboard.createCursorKeys();
     }
 
-    function update()
-    {
-      
+    function update() {
+        playerUpdate();
     }
 
-    function createPlayer(x, y)
-    {
+    function createPlayer(x, y) {
         var player = players.create(x, y, 'playersprite');
+        player.body.colliderWorldBounds = true;
+
+    }
+
+    function playerUpdate() {
+        players.forEach(function (p) {
+            p.body.velocity.x = 0;
+            if (inputs.left.isDown) {
+                p.body.velocity.x = -150;
+            }
+            else if (inputs.right.isDown) {
+                p.body.velocity.x = 150;
+            }
+        });
     }
 };
