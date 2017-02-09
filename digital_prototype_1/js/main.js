64941ac80581a9b8edefd3ebceb38a9e5c678254
Nuragami <michaelzhang.a@gmail.com>
2017-02-08 19:16:02 -0500

    rr5

--- digital_prototype_1/js/main.js
+++ digital_prototype_1/js/main.js
@@ -5,14 +5,20 @@ window.onload = function ()
     var game = new Phaser.Game(400, 800, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
     
     var background;
+
     var player;
     var cursors;
+
+    var bullets;
+    var bulletTime = 0;
+    var fireButton;
  
 
     function preload()
     {
         game.load.image('playersprite', "assets/playersprite.png");
         game.load.image('background', "assets/background.png");
+        game.load.image('bullet', "assets/bullet.png")
     }
 
     function create()
@@ -24,6 +30,16 @@ window.onload = function ()
 
         cursors = game.input.keyboard.createCursorKeys();
 
+        bullets = game.add.group();
+        bullets.enableBody = true;
+        bullets.physicsBodyType = Phaser.Physics.ARCADE;
+        bullets.createMultiple(30, 'bullet');
+        bullets.setAll('anchor.x', 0.5);
+        bullets.setAll('anchor.y', 1);
+        bullets.setAll('outofBoundsKill', true);
+        bullets.setAll('checkWorldBounds', true);
+
+        fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         
        
 
@@ -33,6 +49,9 @@ window.onload = function ()
     {
         background.tilePosition.y += 2;
 
+        player.body.velocity.x = 0;
+        player.body.colliderWorldBounds = true;
+
         if(cursors.left.isDown)
         {
             player.body.velocity.x = -350;
@@ -41,7 +60,35 @@ window.onload = function ()
         {
             player.body.velocity.x = 350;
         }
+        if (cursors.up.isDown)
+        {
+            player.body.velocity.y = -300
+        }
+        if (cursors.down.isDown)
+        {
+            player.body.velocity.y = 300;
+        }
+           
+
+        if(fireButton.isDown)
+        {
+            fireBullet();
+        }
       
+    }
+
+    function fireBullet()
+    {
+        if(game.time.now > bulletTime)
+        {
+            bullet = bullets.getFirstExists(false);
+            if(bullet)
+            {
+                bullet.reset(player.x, player, y);
+                bullet.body.velocity.y = -400;
+                bulletTime = game.time.now + 200;
+            }
+        }
     }
 
    
