63e6869a7798dc6bd4cd205fb85f7ae2ef192b22
Nuragami <michaelzhang.a@gmail.com>
2017-02-08 21:04:16 -0500

    rrr6.4

--- digital_prototype_1/js/main.js
+++ digital_prototype_1/js/main.js
@@ -87,6 +87,7 @@ window.onload = function ()
 
         winTextCowboy = game.add.text(game.world.centerX-100, game.world.centerY, "Cowboy Win!", { font: '32px Arial', fill: '#fff' });
         winTextCowboy.visible = false;
+
         winTextAsteriod = game.add.text(game.world.centerX-100, game.world.centerY, "Asteriod Win!", { font: '32px Arial', fill: '#fff' });
         winTextAsteriod.visible = false;
 
@@ -152,7 +153,7 @@ window.onload = function ()
             var bullet = cowboyBullets.getFirstExists(false);
             if(bullet)
             {
-                bullet.reset(cowboy.x + 42, cowboy.y + 14);
+                bullet.reset(cowboy.x + 54, cowboy.y + 14);
                 bullet.body.velocity.x = 400;
                 bulletTime = game.time.now + 200;
             }
