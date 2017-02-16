bf70e7ef7bb93ac3f84132bded048a1ee0e179eb
Nuragami <michaelzhang.a@gmail.com>
2017-02-15 19:26:22 -0500

    r2

--- digital_prototype_2/js/main.js
+++ digital_prototype_2/js/main.js
@@ -2,25 +2,22 @@ window.onload = function ()
 {
     "use strict";
 
-    var game = new Phaser.Game(800, 400, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
     var background;
 
     function preload()
     {
         game.load.image('background', "assets/background.png");
-
     }
 
     function create()
     {
-
-
+        background = game.add.tileSprite(0, 0, 800, 600, 'background');
     }
 
     function update()
     {
 
     }
-
 };
