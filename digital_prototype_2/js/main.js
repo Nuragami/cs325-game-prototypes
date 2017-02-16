c80fec6efa7640e392a87024ff548dee95f857eb
Nuragami <michaelzhang.a@gmail.com>
2017-02-15 20:06:00 -0500

    r3

--- digital_prototype_2/js/main.js
+++ digital_prototype_2/js/main.js
@@ -1,23 +1,33 @@
 window.onload = function ()
 {
-    "use strict";
+  "use strict";
 
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
-    var background;
+  var background;
+  var player;
 
-    function preload()
-    {
-        game.load.image('background', "assets/background.png");
-    }
+  function preload()
+  {
+    game.load.image('background', "assets/background.png");
+    game.load.image('player', "assets/player.png");
+  }
 
-    function create()
-    {
-        background = game.add.tileSprite(0, 0, 800, 600, 'background');
-    }
+  function create()
+  {
+    game.world.setBounds(0, 0, 1600, 600);
+    background = game.add.tileSprite(0, 0, 800, 600, 'background');
+    background.fixedToCamera = true;
 
-    function update()
-    {
+    player = game.add.sprite(0, 0, 'player');
+    player.anchor.setTo(0.5, 0.5);
 
-    }
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+  }
+
+  function update()
+  {
+
+
+  }
 };
